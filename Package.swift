12c481e8b2b4f197ce0042da19b254692414b3ad
FDC.Glynnn-NC-SA-IOS <fdc.glynvile@gmail.com>
2026-02-05 16:26:36 +0800

    Fix Package.swift for SwiftPM

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
+// swift-tools-version: 5.7
 import PackageDescription
 
 let package = Package(
-    name: "OpalImagePicker"
+    name: "OpalImagePicker",
+    defaultLocalization: "en",   // ✅ add this
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(name: "OpalImagePicker", targets: ["OpalImagePicker"])
+    ],
+    targets: [
+        .target(
+            name: "OpalImagePicker",
+            path: "OpalImagePicker"
+            // ideally remove resources: [.process(".")] if you had it
+        )
+    ]
 )
