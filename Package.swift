cc27d1c46b887c42c1f12b36264d15752bcdccc3
fdcglynvile <102502976+fdcglynvile@users.noreply.github.com>
2026-03-03 10:28:27 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,18 +1,26 @@
-// swift-tools-version: 5.7
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "OpalImagePicker",
-    defaultLocalization: "en",   // ✅ add this
-    platforms: [.iOS(.v11)],
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
-        .library(name: "OpalImagePicker", targets: ["OpalImagePicker"])
+        .library(
+            name: "OpalImagePicker",
+            targets: ["OpalImagePicker"]
+        )
     ],
     targets: [
         .target(
             name: "OpalImagePicker",
-            path: "OpalImagePicker"
-            // ideally remove resources: [.process(".")] if you had it
+            path: "OpalImagePicker",
+            resources: [
+                .process("Resources")
+            ],
+            publicHeadersPath: "."
         )
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
